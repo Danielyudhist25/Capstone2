4711edd5fee541111f7838fd4022766c0de0f6cd
Danielyudhist25 <1972056@maranatha.ac.id>
2026-06-02 00:18:08 +0700

    penambahan app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+// require('dotenv').config();
+
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false
+    })
+);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use('/', require('./routes/auth'));
+app.use('/dashboard', require('./routes/dashboard'));
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running`);
+});
